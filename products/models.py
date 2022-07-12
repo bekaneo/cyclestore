0320c0d0beb96305944f077c951ef81836daa8d6
Aman <aman.work.acc@gmail.com>
2022-07-12 16:06:29 +0600

    add color product.model

--- products/models.py
+++ products/models.py
@@ -13,6 +13,8 @@ class Product(models.Model):
     category = models.ForeignKey(Category,
                                  on_delete=models.CASCADE,
                                  related_name='products')
+    colory = models.CharField(max_length=20)
+    
 
     subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, related_name='subcategory')
 
