eed315bc92122dbf0160f3fa95b6f4bcffdf53fb
Aman <aman.work.acc@gmail.com>
2022-07-12 15:40:50 +0600

    add category Size

--- categories/models.py
+++ categories/models.py
@@ -5,7 +5,7 @@ from slugify import slugify
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, blank=True)
-
+    
     def __str__(self):
         return self.name
 
@@ -14,6 +14,18 @@ class Category(models.Model):
             self.slug = slugify(str(self.name))
         super().save(*args, **kwargs)
 
+# class Size(models.Model):
+#     size_in_inch = models.IntegerField()
+#     size_in_cm = models.CharField(max_length=100)
+#     slug = models.SlugField(max_length=100, blank=True)
+
+#     def __str__(self):
+#         return self.size_in_inch
+    
+#     def save(self, *args, **kwargs):
+#         if not self.slug:
+#             self.slug = slugify(str(self.size_in_inch))
+#         super().save(*args, **kwargs)
 
 class SubCategory(models.Model):
     name = models.CharField(max_length=100)
