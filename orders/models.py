ea901d2ca55c5e2559ea4e8a413acf54b650b089
Aman <aman.work.acc@gmail.com>
2022-07-12 16:38:55 +0600

    models orders

--- orders/models.py
+++ orders/models.py
@@ -30,3 +30,5 @@ class OrderItems(models.Model):
     order = models.ForeignKey(Order, on_delete=models.RESTRICT, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.RESTRICT)
     quantity = models.PositiveSmallIntegerField(default=1)
+
+# dfs
