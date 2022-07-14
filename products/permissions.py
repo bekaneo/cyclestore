91cb7842c8b5ad5b5785de5f740a105098677554
bekaneo <vealtto@gmail.com>
2022-07-14 17:21:46 +0600

    permissions

--- products/permissions.py
+++ products/permissions.py
@@ -1,6 +1,14 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.permissions import IsAdminUser
+
+
+#
+# class IsAuthor(BasePermission):
+#     def has_object_permission(self, request, view, obj):
+#         print(obj.user, 'sadsadadasdsasd')
+#         return request.user.is_authenticated and request.user == obj.user
 
 
 class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user == obj.author
+        return bool(request.user == obj.user)
--- products/views.py
+++ products/views.py
@@ -2,9 +2,11 @@ from products.models import Product
 from products.serializers import ProductListSerializer, ProductSerializer
 from rest_framework import permissions
 from rest_framework.filters import SearchFilter
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.views import APIView
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+
+from .permissions import IsAuthor
 
 
 class ProductViewSet(ModelViewSet):
@@ -21,8 +23,11 @@ class ProductViewSet(ModelViewSet):
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
             self.permission_classes = [permissions.AllowAny]
-        if self.action in ['destroy', 'update', 'create', 'partial_update']:
-            self.permission_classes = [permissions.IsAdminUser]
+        if self.action in ['create']:
+            self.permission_classes = [permissions.IsAuthenticated]
+        if self.action in ['destroy', 'update', 'partial_update']:
+            self.permission_classes = [IsAuthor]
+            # print(self.permission_classes[0].has_object_permission())
         return super().get_permissions()
 
     def create(self, request, *args, **kwargs):
