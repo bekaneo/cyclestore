8466ab60995885ff62eeedfad25eb8043e216af3
Beknazar Nurbek <beknazarnurbek@Beknazars-MacBook-Pro.local>
2022-07-21 22:52:44 +0600

    ubrat

--- cycle/settings.py
+++ cycle/settings.py
@@ -207,8 +207,8 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
-    'SLIDING_TOKEN_LIFETIME': timedelta(days=20),
-    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=30),
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
