17fb18d0aa9698f9f0a146878a24b9193f77340d
Beknazar Nurbek <beknazarnurbek@Beknazars-MacBook-Pro.local>
2022-07-21 13:42:26 +0600

    celeryyy

--- cycle/celery.py
+++ cycle/celery.py
@@ -1,11 +1,11 @@
-# import os
-# from celery import Celery
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cycle.settings')
-#
-# app = Celery('cycle')
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-# app.autodiscover_tasks()
-# app.conf.update(result_expires=3600, enable_utc=True, timezone='UTC')
-# app.conf.beat_schedule = {}
-# app.conf.timezone = 'UTC'
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cycle.settings')
+
+app = Celery('cycle')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+app.conf.update(result_expires=3600, enable_utc=True, timezone='UTC')
+app.conf.beat_schedule = {}
+app.conf.timezone = 'UTC'
